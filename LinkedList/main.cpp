b371903a807105a755678da48734560492944b7a
LYWen <1658441344@qq.com>
2025-03-06 19:42:22 +0800

    LinkedList

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -341,9 +341,38 @@ public:
 };
 
 // 9. T142.环形链表II
+/*给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ 不允许修改 链表。*/
+
+/* 假设从头结点到环形入口节点的节点数为x; 环形入口节点到fast指针与slow指针相遇节点数为y; 从相遇节点再到环形入口节点数为z.
+ 有：x+n(y+z)+y=2(x+y)，即：x=n(y+z)-y=(n-1)(y+z)+z.
+ 从头节点/相遇节点出发，到环形入口节点的距离相同。
+ */
+/*时间复杂度:O(n);
+    快慢指针相遇前，指针走的次数小于链表长度，快慢指针相遇后，两个index指针走的次数也小于链表长度，总体为走的次数小于 2n
+ 空间复杂度: O(1).
+ */
 class Solution_9{
-    
-}
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow=head,*fast=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast){     // 快慢指针相遇
+                ListNode *node1=head,*node2=fast;
+                while(node1!=node2){
+                    node1=node1->next;
+                    node2=node2->next;
+                }
+                return node1;
+            }
+        }
+        return nullptr;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
